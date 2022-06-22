8ee78fb82077e64b4329d394f8b657554730ab42
sadrium <38139998+sadrium@users.noreply.github.com>
2022-06-22 18:56:01 +0300

    Add files via upload

--- Sadovskaya_ZBPI213.py
+++ Sadovskaya_ZBPI213.py
@@ -1,114 +1,110 @@
-# 1.
-
+# 1
 def fact(x):
-    result = 1
-    for i in range(1, x + 1):
-        result *= i
-    return result
-
-#2
-
-def filter_even(li):
-    return list(filter(lambda x: True if (x % 2) == 0 else False, li))
-
-#3
-
-def square(li):
-    return list(map(lambda x: x**2, li))
-
-#4
-
-def bin_search(li, element):
-    middle = 0
-    start = 0
-    step = 0
-    end = len(li)
-
-    while (start <= end):
-      step = step + 1
-      mid = (start + end) // 2
-
-      if element < li[mid]:
-        end = mid - 1
-      else:
-        start = mid + 1
-      if element == li[mid]:
-      return mid
-
-    return -1
-
-#5
-
-def is_palindrome(string):
-    string_rev = ''
-    for i in range(len(string) - 1, -1, -1):
-      string_rev += string[i]
-    if string == string_rev:
-      return 'YES'
+    if x > 1:
+        return x*fact(x-1)
     else:
-      return 'NO'
+        return 1
+
+# 2
+def filter_even(li):
+    return list(filter(lambda x:x%2==0, li))
+
+# 3
+def square(li):
+    return list(map(lambda x:x*x, li))
+
+# 4
+def bin_search(li, element):
+    if element in li:
+        return li.index(element)
+    else:
+        return -1
+
+# 5
+def is_palindrome(string):
+    l = 0
+    r = len(string) - 1
+    is_pal = True
+
+    while l < r:
+        if not string[l].isalpha():
+            l+=1
+            continue
+        if not string[r].isalpha():
+            r-=1
+            continue
+        if string[l].lower() == string[r].lower():
+            l+=1
+            r-=1
+        else:
+            is_pal = False
+            break
+
+    return 'YES' if is_pal else 'NO'
 
 # 6
-
 def calculate(path2file):
-    with open(path2file, 'r', encoding='utf-8') as file:
-        to_calc = file.read()
-    answer = ''
-    to_calc = to_calc.split('    ')
-    for each in to_calc:
-        each = each.split(' ')
-        operation = each[0]
-        numbers = [each[1], each[2]]
-        try:
-            numbers = list(map(int, numbers))
-        except:
-            if numbers[0][0] == '-':
-                numbers[0] = -1 * int(numbers[0][1:])
-            if numbers[1][0] == '-':
-                numbers[1] = -1 * int(numbers[1][1:])
-        if operation == '+':
-            answer += str(numbers[0] + numbers[1])
-        elif operation == '-':
-            answer += str(numbers[0] - numbers[1])
-        elif operation == '*':
-            answer += str(numbers[0] * numbers[1])
-        elif operation == '//':
-            answer += str(numbers[0] // numbers[1])
-        elif operation == '%':
-            answer += str(numbers[0] % numbers[1])
-        elif operation == '**':
-            answer += str(numbers[0]** numbers[1])
-        answer += ' '
-    answer = answer[:-1]
-    answer = answer.replace(' ', ', ')
-    return answer
+    operations = {
+        '+': lambda x,y:x+y,
+        '-': lambda x,y:x-y,
+        '*': lambda x,y:x*y,
+        '//': lambda x,y:x//y,
+        '%': lambda x,y:x%y,
+        '**': lambda x,y:x**y,
+    }
+
+    with open(path2file, 'r') as f:
+        lines = f.readlines()
+        res = ''
+
+        for line in range(len(lines)):
+            elems = lines[line].split()
+            res += str(operations[elems[0]](int(elems[1]), int(elems[2])))
+            if line < len(lines)-1:
+                if line < len(lines) - 1:
+                    res += ','
+    return res
 
 # 7
-
 def substring_slice(path2file_1,path2file_2):
-    with open(path2file_1, 'r', encoding='utf-8') as file:
-        strings = file.read()
-        strings = strings.split('\n')
-    with open(path2file_2, 'r', encoding='utf-8') as file:
-        indexes = file.read()
-        indexes = indexes.split('\n')
-    answer = ''
-    for i in range(len(strings)):
-        answer += strings[i][int(indexes[i].split(' ')[0]):int(indexes[i].split(' ')[1]) + 1] + ' '
-    answer = answer[:-1]
-    return answer
+    with open(path2file_1, 'r') as f1:
+        lines1 = f1.readlines()
+        with open(path2file_2, 'r') as f2:
+            lines2 = f2.readlines()
+            res = ''
 
-#8
+            for line in range(len(lines1)):
+                string = lines1[line]
+                positions = lines2[line].split()
 
-def decode_ch(sting_of_elements):
-    periodic_table = json.load(open('periodic_table.json'))
-    for each in periodic_table:
-        if each in sting_of_elements:
-            sting_of_elements = sting_of_elements.replace(each, periodic_table[each])
-    return sting_of_elements
+                res += string[int(positions[0]):int(positions[1])+1]
+                if line < len(lines1) - 1:
+                    res += ' '
 
-#9
+    return res
 
+# 8
+import json
+
+def decode_ch(string_of_elements):
+    periodic_table = json.load(open('periodic_table.json', encoding="utf8"))
+
+    res = ''
+    elem = ''
+    for i in range(len(string_of_elements)):
+        if string_of_elements[i].isupper():
+            if i < len(string_of_elements)-1 and string_of_elements[i+1].islower():
+                elem = string_of_elements[i]
+                continue
+            res += periodic_table[string_of_elements[i]]
+        elif string_of_elements[i].islower():
+            res += periodic_table[elem+string_of_elements[i]]
+
+    return res
+
+print(decode_ch('NOTiFICaTiON'))
+
+# 9
 class Student:
     name = ''
     surname = ''
@@ -142,11 +138,9 @@ class Student:
         else:
             return 'NO'
 
-#10
-
+# 10
 class MyError(Exception):
     msg = ''
 
     def __init__(self, msg):
         self.msg = msg
-
